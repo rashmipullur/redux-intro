f8d4895a37b046123a6a10feb223bbe0502daf47
rashmipullur <mheresince1998@gmail.com>
2025-06-16 17:20:04 +0530

    13# the redux devtools

--- src/features/customers/customerSlice.js
+++ src/features/customers/customerSlice.js
@@ -13,7 +13,7 @@ export default function customerReducer(state = initialStateCustomer, action) {
         nationalID: action.payload.nationalID,
         createdAt: action.payload.createdAt,
       };
-    case "account/updateName":
+    case "customer/updateName":
       return { ...state, fullName: action.payload };
 
     default:
@@ -29,5 +29,5 @@ export function createCustomer(fullName, nationalID) {
 }
 
 export function updateName(fullName) {
-  return { type: "account/updateName", payload: fullName };
+  return { type: "customer/updateName", payload: fullName };
 }
